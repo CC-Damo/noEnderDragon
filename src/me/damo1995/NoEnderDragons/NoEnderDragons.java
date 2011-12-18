787bf329fe5598ef25aaf84dd551a3c76f10b128
Damien Delay <damien.delay1995@gmail.com>
2011-12-18 00:09:25 +0000

    Version 1.1: Added End Spawn Variable in config, Delete old config before installation, Updated Plugin.yml to reflect author and description. added enable notifications to reflect config variables

--- src/me/damo1995/NoEnderDragons/NoEnderDragons.java
+++ src/me/damo1995/NoEnderDragons/NoEnderDragons.java
@@ -3,14 +3,12 @@ package me.damo1995.NoEnderDragons;
 import java.io.File;
 import java.util.logging.Logger;
 
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Event;
 import org.bukkit.event.Event.Priority;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-@SuppressWarnings("unused")
 public class NoEnderDragons extends JavaPlugin {
 	
 	public final Logger log = Logger.getLogger("Minecraft");
@@ -21,15 +19,38 @@ public class NoEnderDragons extends JavaPlugin {
 	public NoEnderDragonConfig config;
 	
 	public void onEnable(){
+		//Register the events.//
+		
 		PluginManager pm = getServer().getPluginManager();
 		pm.registerEvent(Event.Type.ENTITY_EXPLODE, this.DragonListen, Priority.Highest, this);
 		pm.registerEvent(Event.Type.CREATURE_SPAWN, this.DragonListen, Priority.Highest, this);
 		
+		//Setup Configuration. //
 		this.pluginDirPath = this.getDataFolder().getAbsolutePath();
 		this.configFile = new File(this.pluginDirPath + File.separator + "config.yml");
 		this.config = new NoEnderDragonConfig(this.configFile);
 		
-		
+		if(this.config.getBooliean("block-damage") == false){
+			this.logMessage("Block Damage damage is Disabled");
+		}
+		else
+		{
+			this.logMessage("Block Damage is Enabled!");
+		}
+		if(this.config.getBooliean("block-dragons") == true){
+			this.logMessage("EnderDragons are Blocked from spawning");
+		}
+		else
+		{
+			this.logMessage("EnderDragons can spawn as normal");
+		}
+		if(this.config.getBooliean("allow-end") == true){
+			this.logMessage("EnderDragons can spawn in the end!");
+		}
+		else
+		{
+			this.logMessage("EnderDragons are Disabled in the end");
+		}
 		this.logMessage("Enabled");
 		
 	}
